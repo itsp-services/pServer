9b27d6e0088372a54331aab81d18e180760dc35b
Andreas <andreas.schlager@htl-salzburg.ac.at>
2019-09-18 09:28:00 +0200

    Added wrapper class around ids.

---
+++ src/ItspServices.pServer.Abstraction/Models/Id.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItspServices.pServer.Abstraction.Models
+{
+    public class Id
+    {
+        public int Value { get; set; }
+        public bool IsValid { get; set; } = false;
+
+        public static implicit operator Id(int id)
+        {
+            return new Id { Value = id, IsValid = true };
+        }
+
+        public static implicit operator int(Id id)
+        {
+            return id.Value;
+        }
+    }
+}
