6778e98c873fb4329e900a9a0b06a4c244d24dd7
Andreas <andreas.schlager@htl-salzburg.ac.at>
2019-09-18 09:33:41 +0200

    Remove unnecessary using statements.

--- src/ItspServices.pServer.Abstraction/Models/Id.cs
+++ src/ItspServices.pServer.Abstraction/Models/Id.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ItspServices.pServer.Abstraction.Models
+﻿namespace ItspServices.pServer.Abstraction.Models
 {
     public class Id
     {
