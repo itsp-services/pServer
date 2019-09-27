c2d6aedfc6f22160157b4a9e184a283ace299298
Matthias Auswoeger <Matthias.Auswoeger@intertops.ag>
2019-09-27 15:38:29 +0200

    Add 'Set' method to client.

---
+++ src/ItspServices.pServer.Client/ProtectedDataClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using ItspServices.pServer.Client.Model;
+using ItspServices.pServer.Client.RestApi;
+
+namespace ItspServices.pServer.Client
+{
+    public class ProtectedDataClient
+    {
+        private RestApiClient _restClient;
+
+        public ProtectedDataClient(IHttpClientFactory factory)
+        {
+            _restClient = new RestApiClient(factory);
+        }
+
+        public async Task Set(string destination, string protectedData)
+        {
+            FolderModel root = await _restClient.RequestFolderById(null);
+            string folderPath = destination.Substring(0, destination.LastIndexOf('/'));
+            FolderModel folder = await FindFolder("", folderPath, root);
+
+            // TODO: request secrets
+        }
+
+        private async Task<FolderModel> FindFolder(string path, string destination, FolderModel currentFolder)
+        {
+            if (currentFolder.Name != "root")
+                path += '/' + currentFolder.Name;
+            if (path == destination)
+                return currentFolder;
+            foreach (int folderId in currentFolder.SubfolderIds)
+            {
+                return await FindFolder(path, destination, await _restClient.RequestFolderById(folderId));
+            }
+            return null;
+        }
+    }
+}
---
+++ test/ItspServices.pServer.ClientTest/ProtectedDataClientTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using ItspServices.pServer.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ItspServices.pServer.ClientTest
+{
+    [TestClass]
+    public class ProtectedDataClientTests
+    {
+        #region nested classes
+        class MockHttpMessageHandler : HttpMessageHandler
+        {
+            public Func<HttpRequestMessage, HttpResponseMessage> Callback { get; set; }
+
+            public MockHttpMessageHandler(Func<HttpRequestMessage, HttpResponseMessage> callback)
+            {
+                Callback = callback;
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+                => Task.FromResult(Callback(request));
+        }
+        #endregion
+
+        [TestMethod]
+        public async Task SaveProtectedData_ShouldSucceed()
+        {
+            bool called = false;
+
+            Mock<IHttpClientFactory> clientFactory = new Mock<IHttpClientFactory>();
+            HttpResponseMessage Callback(HttpRequestMessage request)
+            {
+                called = true;
+                // TODO: Add foldermodel to content
+                return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            }
+            clientFactory
+                .Setup(x => x.CreateClient(It.IsAny<string>()))
+                .Returns(
+                    new HttpClient(new MockHttpMessageHandler(Callback))
+                    {
+                        BaseAddress = new Uri("http://test.com")
+                    });
+
+            ProtectedDataClient client = new ProtectedDataClient(clientFactory.Object);
+            await client.Set("/Andys Passwords/MailAccount", "SecretPassword");
+
+            Assert.IsTrue(called);
+        }
+    }
+}
