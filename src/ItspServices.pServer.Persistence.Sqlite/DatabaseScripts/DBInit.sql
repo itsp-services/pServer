8c0da6d140ffb09a6c39aa9310edb2ddf17d1a28
Andi1603 <andreas.schlager28@gmail.com>
2019-10-04 16:12:53 +0200

    Update comment in SQL script.

--- src/ItspServices.pServer.Persistence.Sqlite/DatabaseScripts/DBInit.sql
+++ src/ItspServices.pServer.Persistence.Sqlite/DatabaseScripts/DBInit.sql
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS ProtectedData (
 CREATE TABLE IF NOT EXISTS ProtectedDataPermissions (
   UserID INT NOT NULL,
   DataID INT NOT NULL,
-  Permission INT NOT NULL,
+  Permission INT NOT NULL, -- 1=View, 2=Read, 3=Write, ...
   PRIMARY KEY (UserID, DataID),
   CONSTRAINT fk_ProtectedDataPermissions_User
     FOREIGN KEY (UserID)
