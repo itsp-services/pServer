5c0c0cd847b123b99cba17e098783ebd07786252
Andi1603 <andreas.schlager28@gmail.com>
2019-10-05 16:45:47 +0200

    Add FolderRepositoryTests.

---
+++ test/ItspServices.pServer.ServerTest/Persistence.SqliteTests/FolderRepositoryTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ItspServices.pServer.ServerTest.Persistence.SqliteTests
+{
+    [TestClass]
+    public class FolderRepositoryTests
+    {
+    }
+}
