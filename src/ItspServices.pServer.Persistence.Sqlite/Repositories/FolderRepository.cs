b684300d98de4fb7b3167c87b1046eb38d98bf01
Benjamin Bayer <benjamin.bayer@htl-salzburg.ac.at>
2019-10-08 14:57:41 +0200

    Create FolderRepository tests.

---
+++ src/ItspServices.pServer.Persistence.Sqlite/Repositories/FolderRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using ItspServices.pServer.Abstraction.Models;
+using ItspServices.pServer.Abstraction.Repository;
+using ItspServices.pServer.Abstraction.Units;
+
+namespace ItspServices.pServer.Persistence.Sqlite.Repositories
+{
+    class FolderRepository : IRepository<Folder, int>
+    {
+
+        private DbProviderFactory _sqlFactory;
+        private readonly string _connectionString;
+
+        public FolderRepository(DbProviderFactory sqlFactory, string connectionString)
+        {
+            _sqlFactory = sqlFactory;
+            _connectionString = connectionString;
+        }
+
+        public Folder GetById(int id)
+        {
+            return new Folder() {  };
+        }
+
+        public IEnumerable<Folder> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAddUnitOfWork<Folder> Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRemoveUnitOfWork<Folder, int> Remove(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUpdateUnitOfWork<Folder, int> Update(int key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- test/ItspServices.pServer.ServerTest/Persistence.SqliteTests/FolderRepositoryTests.cs
+++ test/ItspServices.pServer.ServerTest/Persistence.SqliteTests/FolderRepositoryTests.cs
@@ -1,9 +1,72 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Data.Common;
+using System.IO;
+using System.Reflection;
+using ItspServices.pServer.Abstraction.Models;
+using ItspServices.pServer.Persistence.Sqlite.Repositories;
+using Microsoft.Data.Sqlite;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ItspServices.pServer.ServerTest.Persistence.SqliteTests
 {
     [TestClass]
     public class FolderRepositoryTests
     {
+        private TestContext _testContext;
+        public TestContext TestContext
+        {
+            get { return _testContext; }
+            set { _testContext = value; }
+        }
+
+        DbConnection memoryDbConnection;
+        FolderRepository repository;
+
+        #region Initialize and cleanup methods
+
+        [TestInitialize]
+        public void Init()
+        {
+            string testConnectionString = $"Data Source={_testContext.TestName}; Mode=Memory; Cache=Shared";
+            memoryDbConnection = SqliteFactory.Instance.CreateConnection();
+            memoryDbConnection.ConnectionString = testConnectionString;
+            // A connection has to be open during the tests so the in-memory
+            // database will not be freed each time the repository closes the connection.
+            memoryDbConnection.Open();
+            InitSchema(memoryDbConnection);
+            repository = new FolderRepository(SqliteFactory.Instance, testConnectionString);
+        }
+
+        private static void InitSchema(DbConnection con)
+        {
+            Assembly assembly = typeof(UserRepository).GetTypeInfo().Assembly;
+            Stream schemaSqlResource = assembly.GetManifestResourceStream("ItspServices.pServer.Persistence.Sqlite.DatabaseScripts.DBInit.sql");
+            DbCommand initSchema = SqliteFactory.Instance.CreateCommand();
+            initSchema.Connection = con;
+            using (TextReader reader = new StreamReader(schemaSqlResource))
+                initSchema.CommandText = reader.ReadToEnd();
+            initSchema.ExecuteNonQuery();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            repository = null;
+            memoryDbConnection.Close();
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void CreateRootFolderTest_ShouldSucced()
+        {
+            DbCommand command = memoryDbConnection.CreateCommand();
+            command.CommandText = "INSERT INTO Folders('ID', 'Foldername', 'Parent') VALUES "
+                                   + "(0, 'root', 0)";
+
+            command.ExecuteNonQuery();
+
+            Folder root = repository.GetById(0);
+
+        }
     }
 }
