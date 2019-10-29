9ea0e6fc0fad789d5a30af1f8f8125faf33d10b8
Andi1603 <andreas.schlager28@gmail.com>
2019-10-29 15:00:19 +0100

    Refactor AddUserUnitOfWork

--- src/ItspServices.pServer.Persistence.Sqlite/Units/UserUnits/AddUserUnitOfWork.cs
+++ src/ItspServices.pServer.Persistence.Sqlite/Units/UserUnits/AddUserUnitOfWork.cs
@@ -17,55 +17,80 @@ namespace ItspServices.pServer.Persistence.Sqlite.Units.UserUnits
 
         protected override void Complete(DbConnection con)
         {
+            if (!UserAlreadyExists(con))
+            {
+                InsertUser(con);
+                if (Entity.HasKeys())
+                {
+                    InsertPublicKeys(con);
+                }
+            }
+        }
+
+        private bool UserAlreadyExists(DbConnection con)
+        {
+            using (DbCommand query = con.CreateCommand())
+            {
+                query.AddParameterWithValue("username", Entity.UserName);
+                query.CommandText = "SELECT Username FROM Users " +
+                                    "WHERE Users.Username=@username;";
+                using (IDataReader reader = query.ExecuteReader())
+                {
+                    return reader.Read();
+                }
+            }
+        }
+
+        private void InsertUser(DbConnection con)
+        {
             using (DbCommand insert = con.CreateCommand())
             {
                 insert.AddParameterWithValue("username", Entity.UserName);
                 insert.AddParameterWithValue("password", Entity.PasswordHash);
                 insert.AddParameterWithValue("role", Entity.Role);
 
-                insert.CommandText = "SELECT Username FROM Users " +
-                                     "WHERE Users.Username=@username;";
-                using (IDataReader reader = insert.ExecuteReader())
+                insert.CommandText = "INSERT INTO Users(Username, PasswordHash, RoleID) " +
+                         "SELECT N, P, ID FROM(SELECT @username AS N, @password AS P) " +
+                         "JOIN Roles ON Roles.Name=@role;";
+                insert.ExecuteNonQuery();
+            }
+        }
+
+        private void InsertPublicKeys(DbConnection con)
+        {
+            using (DbCommand insert = con.CreateCommand())
+            {
+                int userID = FindUserId(con);
+                insert.CommandText = "INSERT INTO PublicKeys ('UserID', 'PublicKeyNumber', 'KeyData', 'Active') VALUES ";
+                for (int i = 0; i < Entity.PublicKeys.Count; i++)
                 {
-                    if(reader.Read())
+                    insert.AddParameterWithValue($"keydata{i}", Entity.PublicKeys[i].AsBase64String());
+                    int active = (Entity.PublicKeys[i].Flag == Key.KeyFlag.ACTIVE) ? 1 : 0;
+
+                    insert.CommandText += $"({userID}, {i + 1}, @keydata{i}, {active})";
+                    if (i >= Entity.PublicKeys.Count - 1)
                     {
-                        // TODO: Log
-                        return;
+                        insert.CommandText += ';';
+                    }
+                    else
+                    {
+                        insert.CommandText += ',';
                     }
                 }
-
-                insert.CommandText = "INSERT INTO Users(Username, PasswordHash, RoleID) " +
-                                     "SELECT N, Pw, ID FROM(SELECT @username AS N, @password AS Pw) " +
-                                     "JOIN Roles ON Roles.Name=@role;";
                 insert.ExecuteNonQuery();
+            }
+        }
 
-
-                if (Entity.HasKeys())
+        private int FindUserId(DbConnection con)
+        {
+            using (DbCommand query = con.CreateCommand())
+            {
+                query.AddParameterWithValue("username", Entity.UserName);
+                query.CommandText += "SELECT ID FROM Users WHERE Users.UserName=@username;";
+                using (IDataReader reader = query.ExecuteReader())
                 {
-                    insert.CommandText = "SELECT ID FROM Users WHERE Users.UserName=@username;";
-                    int userID = -1;
-                    using (IDataReader reader = insert.ExecuteReader())
-                    {
-                        reader.Read();
-                        userID = reader.GetInt32(0);
-                    }
-                    insert.CommandText = "INSERT INTO PublicKeys ('UserID', 'PublicKeyNumber', 'KeyData', 'Active') VALUES ";
-                    for (int i = 0; i < Entity.PublicKeys.Count; i++)
-                    {
-                        insert.AddParameterWithValue($"keydata{i}", Entity.PublicKeys[i].AsBase64String());
-                        int active = (Entity.PublicKeys[i].Flag == Key.KeyFlag.ACTIVE) ? 1 : 0;
-
-                        insert.CommandText += $"({userID}, {i + 1}, @keydata{i}, {active})";
-                        if (i >= Entity.PublicKeys.Count - 1)
-                        {
-                            insert.CommandText += ';';
-                        }
-                        else
-                        {
-                            insert.CommandText += ',';
-                        }
-                    }
-                    insert.ExecuteNonQuery();
+                    reader.Read();
+                    return reader.GetInt32(0);
                 }
             }
         }
