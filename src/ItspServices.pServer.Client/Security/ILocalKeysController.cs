91a244324bcd2503ce45893491c07210fad644e6
Maxi Pulstorm <maxipulstorm@aon.at>
2019-11-13 18:11:38 +0100

    Add LocalKeysController that saves and provides locally stored Keys.

---
+++ src/ItspServices.pServer.Client/Security/ILocalKeysController.cs
@@ -0,0 +1,11 @@
+﻿using ItspServices.pServer.Client.Models;
+
+namespace ItspServices.pServer.Client.Security
+{
+    public interface ILocalKeysController
+    {
+        string CreateSymmetricKey();
+        string GetPublicKey();
+        string GetPrivateKey();
+    }
+}
