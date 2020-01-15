086c119687d65b742a2e7c95669aa637135d7ee9
Andi1603 <andreas.schlager28@gmail.com>
2020-01-15 11:40:37 +0100

    Add adapter classes

---
+++ src/ItspServices.pServer/Stores/SignInManagerAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ItspServices.pServer.Abstraction.Models;
+using ItspServices.pServer.Abstraction.UseCase.Account;
+
+namespace ItspServices.pServer.Stores
+{
+    public class SignInManagerAdapter : ISignInManager
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public SignInManagerAdapter(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> PasswordSignInAsync(string username, string password)
+        {
+            SignInResult result = await _signInManager.PasswordSignInAsync(username, password, false, false);
+            return result.Succeeded;
+        }
+
+        public async Task<bool> SignOutAsync()
+        {
+            await _signInManager.SignOutAsync();
+            return true;
+        }
+    }
+}
---
+++ src/ItspServices.pServer/Stores/UserManagerAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using ItspServices.pServer.Abstraction.Models;
+using ItspServices.pServer.Abstraction.UseCase.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace ItspServices.pServer.Stores
+{
+    public class UserManagerAdapter : IUserManager
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserManagerAdapter(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<bool> CreateUserAsync(string username, string password)
+        {
+            User user = new User()
+            {
+                UserName = username,
+                NormalizedUserName = username.ToUpper()
+            };
+            IdentityResult result = await _userManager.CreateAsync(user, password);
+            return result.Succeeded;
+        }
+    }
+}
