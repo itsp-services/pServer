82270dc3ab2b3b27e5d5cb7f596bc414e5befb88
Maxi Pulstorm <maxipulstorm@aon.at>
2020-01-15 10:44:17 +0100

    Add Key and KeyPair datatype to make type conversions clearer and easier

---
+++ src/ItspServices.pServer.Client/Datatypes/KeyPair.cs
@@ -0,0 +1,33 @@
+﻿using ItspServices.pServer.Client.Models;
+
+namespace ItspServices.pServer.Client.Datatypes
+{
+    internal class KeyPair
+    {
+        public Key PublicKey { get; set; }
+        public Key SymmetricKey { get; set; }
+
+        public KeyPair() { }
+
+        public KeyPair(KeyPairModel keyPairModel)
+        {
+            PublicKey = new Key(keyPairModel.PublicKey);
+            SymmetricKey = new Key(keyPairModel.SymmetricKey);
+        }
+
+        public void InitializeWithKeyPairModel(KeyPairModel keyPairModel)
+        {
+            this.PublicKey = new Key(keyPairModel.PublicKey);
+            this.SymmetricKey = new Key(keyPairModel.SymmetricKey);
+        }
+
+        public KeyPairModel ToKeyPairModel()
+        {
+            return new KeyPairModel
+            {
+                PublicKey = this.PublicKey.GetBase64(),
+                SymmetricKey = this.SymmetricKey.GetBase64()
+            };
+        }
+    }
+}
